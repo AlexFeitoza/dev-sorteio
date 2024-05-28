90d896eaaaeff5e99cadf9db195d0063a4e7e561
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-05-28 05:29:53 -0300

    end

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 
-const abacate = ("love")
-console.log(abacate)
+function generateNumber(){
+    
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    alert(result)
+}
+
